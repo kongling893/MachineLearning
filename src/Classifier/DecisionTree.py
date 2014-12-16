9e8c0c421d7256f42307a1c7ecf185f0931e4a52
kongling893 <kongling893@163.com>
2014-12-15 23:26:11 -0600

    fix a bug

--- src/Classifier/DecisionTree.py
+++ src/Classifier/DecisionTree.py
@@ -13,8 +13,8 @@ Data struct:
 
 How to use:
         >import DecisionTree
-        >DTree = DecisionTree.createTree(dataset)
-        >label = DecisionTree.classify(DTree,testDataVec)
+        >DTree = DecisionTree.DecisionTree().createTree(dataset)
+        >label = DecisionTree.DecisionTree().classify(DTree,testDataVec)
         >
 
 author: Wei Qiao
@@ -32,18 +32,18 @@ class DecisionTree:
 
         def createTree(self,dataset):
                 labelsList = [datavec["label"] for datavec in dataset]
-                majority = self._majority(labelsList)
+                majority = self.majority(labelsList)
                 if labelsList.count([labelsList[0]]) == len(labelsList):
                         return labelsList[0]
-                if len(dataset[0]["val"]) == 1:
+                if len(dataset[0]["val"]) == 0:
                         return majority
-                bestFeature = self._optimalSplitingFeature(dataset)
+                bestFeature = self.optimalSplitingFeature(dataset)
                 if bestFeature ==-1: return majority
                 DTree = {bestFeature:{}}
                 featureValues = [datavec["val"][bestFeature] for datavec in dataset] 
                 uniqueValues = set(featureValues)
                 for value in uniqueValues:
-                        subdataset = self._splitDataset(dataset,bestFeature,value)
+                        subdataset = self.splitDataset(dataset,bestFeature,value)
                         DTree[bestFeature][value] = self.createTree(subdataset)
                 return DTree
 
@@ -52,12 +52,16 @@ class DecisionTree:
                 firstLevel = DTree.keys()[0]
                 secondLevel = DTree[firstLevel]
                 for key in secondLevel.keys():
-                        if type(secondLevel[key]).__name__ !='dict':
-                                classifiedLabel = secondLevel[key]
-                        else: classifiedLabel = self.classify(secondLevel[key],data) 
+                        if data[firstLevel] == key:
+                                if type(secondLevel[key]).__name__ !='dict':
+                                        classifiedLabel = secondLevel[key]
+                                else: 
+                                        Ndata = deepcopy(data[:firstLevel])
+                                        Ndata.extend(data[firstLevel+1:])
+                                        classifiedLabel = self.classify(secondLevel[key],Ndata) 
                 return classifiedLabel
 
-        def _entropy(self,dataset):
+        def entropy(self,dataset):
                 #Calculates the entropy of the given dataset
                 n = len(dataset)
                 labelSet = {}    #Record all the labels and their corresponding counts
@@ -73,7 +77,7 @@ class DecisionTree:
                         Entropy -= prob*log(prob,2)
                 return Entropy
 
-        def _splitDataset(self,dataset,index,value):
+        def splitDataset(self,dataset,index,value):
                 #Splits the dataset based on given Feature index and Value
                 remainedData = []
                 for datavec in dataset:
@@ -84,10 +88,10 @@ class DecisionTree:
                                 remainedData.append(newdatavec)
                 return remainedData
 
-        def _optimalSplitingFeature(self,dataset):
+        def optimalSplitingFeature(self,dataset):
                 #Finds the optimal feature index to split the dataset
-                N = len(dataset[0]["val"]) - 1
-                oldEntropy = self._entropy(dataset)
+                N = len(dataset[0]["val"])
+                oldEntropy = self.entropy(dataset)
                 bestFeature = -1
                 bestInforGain = 0.0
                 for i in range(N):
@@ -95,16 +99,17 @@ class DecisionTree:
                         uniquevalues = set(featlists)
                         newEntropy = 0.0
                         for value in uniquevalues:
-                                splittedDataset = self._splitDataset(dataset,i,value)
+                                splittedDataset = self.splitDataset(dataset,i,value)
                                 prob = len(splittedDataset)/float(len(dataset))
-                                newEntropy += prob*self._entropy(splittedDataset)
+                                newEntropy += prob*self.entropy(splittedDataset)
+                        
                         inforGain = oldEntropy - newEntropy
                         if inforGain > bestInforGain:
                                 bestInforGain = inforGain
                                 bestFeature = i
                 return bestFeature
 
-        def _majority(self,labels):
+        def majority(self,labels):
                 labelCount = {}
                 for label in labels:
                         if label not in labelCount.keys():
--- src/Classifier/test.py
+++ src/Classifier/test.py
@@ -1,12 +1,12 @@
  
-x1={"val":[1,1,1],"label":"00"}
-x2={"val":[1,1,0],"label":"11"}
-x3={"val":[1,0,1],"label":"11"}
-x4={"val":[0,1,1],"label":"00"}
-x5={"val":[1,0,0],"label":"00"}
-x6={"val":[0,0,1],"label":"00"}
-x7={"val":[0,1,0],"label":"00"}
-x8={"val":[0,0,0],"label":"00"}
+x1={"val":[1,1,1],"label":"0"}
+x2={"val":[1,1,0],"label":"0"}
+x3={"val":[1,0,1],"label":"1"}
+x4={"val":[0,1,1],"label":"0"}
+x5={"val":[1,0,0],"label":"1"}
+x6={"val":[0,0,1],"label":"1"}
+x7={"val":[0,1,0],"label":"1"}
+x8={"val":[0,0,0],"label":"1"}
 
 
 import DecisionTree
@@ -19,11 +19,24 @@ x.append(x4)
 x.append(x5)
 x.append(x6)
 x.append(x7)
-x.append(x8)
+#x.append(x8)
 
 
 DTree = DecisionTree.DecisionTree().createTree(x)
 print DTree
-x9 = x1["val"]
-label = DecisionTree.DecisionTree().classify(DTree,x)
+
+x9 = x8["val"]
+label = DecisionTree.DecisionTree().classify(DTree,x9)
 print label
+'''
+E = DecisionTree.DecisionTree().entropy(x)
+print E
+
+
+sub = DecisionTree.DecisionTree().optimalSplitingFeature(x)
+print sub
+
+sub = DecisionTree.DecisionTree().majority([1,1,1,1,0,0,0,0,3])
+print sub
+'''
+
